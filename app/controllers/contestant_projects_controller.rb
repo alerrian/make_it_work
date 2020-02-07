14ac286803a8cd3b5e9b3a8b03d190729df622b2
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-07 10:50:29 -0700

    Fix wording and readability

--- app/controllers/contestant_projects_controller.rb
+++ app/controllers/contestant_projects_controller.rb
@@ -1,11 +1,11 @@
 class ContestantProjectsController < ApplicationController
   def create
-    ContestantProject.create!(contestant_project_params)
+    ContestantProject.create!(c_p_params)
     redirect_to "/projects/#{params[:project_id]}"
   end
 
   private
-    def contestant_project_params
+    def c_p_params
       params.permit(
         :contestant_id,
         :project_id
