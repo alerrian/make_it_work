3a5b68d454c663d28e77d02109cc427299249d6e
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-02-07 10:55:00 -0700

    Fix test for contestant index

--- spec/features/contestants/contestant_index_page_spec.rb
+++ spec/features/contestants/contestant_index_page_spec.rb
@@ -1,14 +1,3 @@
-# As a visitor,
-# When I visit the contestants index page ("/contestants")
-# I see a list of names of all the contestants
-# And under each contestants name I see a list of the projects (names) that they've been on
-
-# (e.g.   Kentaro Kameyama
-#         Projects: Litfit, Rug Tuxedo
-
-#         Jay McCarroll
-#         Projects: LeatherFeather)
-
 require 'rails_helper'
 
 RSpec.describe 'As a visitor', type: :feature do
@@ -54,6 +43,8 @@ RSpec.describe 'As a visitor', type: :feature do
   it 'can see projects for each contestant' do
     visit '/contestants'
 
-    expect(page).to have_content("Projects: #{@project.name}")
+    within "#contestant-#{@contestant1.id}" do
+      expect(page).to have_content("Projects: #{@project.name}")
+    end
   end
 end
